361a545bbf2f41b76f8f2483b7bc842b2a6363db
Youssef Tarek <youssef.tarek1090@gmail.com>
2025-03-26 16:44:53 +0200

    Initial commit or upda

--- challenge_5/src/views/thankyou.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex flex-col gap-5 h-full items-center justify-center ">
-        <div class="sm:w-2/12 w-full "> 
-            <img src="../assets/assets/images/icon-thank-you.svg" class="w-full" alt="">
-        </div>
-    
-        <div class="font-bold text-3xl">Thank you!</div>
-    </div>
-</template>
